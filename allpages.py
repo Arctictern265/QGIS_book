89a1ae2ed9b77c1c9e5f05cacfada4f426e712df
Takayuki Mizutani <mizutani.takayuki@gmail.com>
2014-07-01 23:51:11 +0900

    create all.md and allpages.py

---
+++ allpages.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+UTF8_BOM = bytearray([0xEF, 0XBB, 0XBF])
+def strip_bom(s):
+    if s.startswith(UTF8_BOM):
+        return s[len(UTF8_BOM):]
+    return s
+
+
+fr = open("list.md","r")
+with open("all.md",'w'):
+ pass
+fw = open("all.md",'a')
+for line in fr:
+   path = line.rstrip("\n")
+   dir = path.strip("/")[0] 
+   try:
+      f = open(path,"r")
+      data = strip_bom(f.read()).replace("img/",dir + "/img/")
+      fw.write(data+"\n\n")
+      f.close()
+   except:
+      pass
+fr.close()
+fw.close()
